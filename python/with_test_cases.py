d7065d0ae1180a3d22fbadc1c81541243ccdb63b
SahilRaj1 <raj24sahil@gmail.com>
2022-08-29 23:39:10 +0530

    commit

---
+++ python/with_test_cases.py
@@ -0,0 +1,15 @@
+""" Sahil Raj """
+
+import sys
+import math
+import collections
+
+sys.stdin= (open('input.txt','r'))
+sys.stdout = (open('output.txt','w'))
+sys.stderr = (open('error.txt','w'))
+
+def solve():
+    pass
+
+for _ in range(int(input())):
+    solve()
---
+++ python/without_test_cases.py
@@ -0,0 +1,11 @@
+""" Sahil Raj """
+
+import sys
+import math
+import collections
+
+sys.stdin= (open('input.txt','r'))
+sys.stdout = (open('output.txt','w'))
+sys.stderr = (open('error.txt','w'))
+
+# Your code starts here
